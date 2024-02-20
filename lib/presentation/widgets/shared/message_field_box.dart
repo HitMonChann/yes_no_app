111c1aa26aa5decbc808648755c93d96ee0c9a36
HitMonChann <manuelsanchezsicilia44@gmail.com>
2024-02-20 12:49:59 +0100

    [WIP] Lógica para la respuesta de la API y mostrarla en la lista de mensajes

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +26,7 @@ class MessageFieldBox extends StatelessWidget {
         onPressed: () {
           final textValue = textController.value.text;
           textController.clear();
+          onValue(textValue);
         },
         icon: const Icon(Icons.send),
       ),
@@ -39,6 +42,7 @@ class MessageFieldBox extends StatelessWidget {
       onFieldSubmitted: (value) {
         textController.clear();
         focusNode.requestFocus();
+        onValue(value);
       },
     );
   }
